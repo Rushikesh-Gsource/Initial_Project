228142adc5723d762ceeef2e095c02e49df19461
Rishikesh Yeslote <30007@gsource.com>
2026-02-05 11:40:47 +0530

    changes

--- src/Component/BlogList.tsx
+++ src/Component/BlogList.tsx
@@ -3,21 +3,25 @@ import { deleteBlog as deleteBlogAction } from "../blogSlice"
 
 export default function BlogList() {
     const blogs = useSelector((state: any) => state.blog.blogs)
+    const user = useSelector((state: any) => state.blog.user)
     const dispatch = useDispatch()
     const handleDelete = (id: any) => {
         dispatch(deleteBlogAction(id))
     }
     return (
         <>
-            <ul>
+            <div>
                 {blogs.map((blog: any) => (
-                    <li key={blog.id}>
-                        <h1>{blog.title}</h1>
-                        <h1>{blog.id}</h1>
-                        <button onClick={() => handleDelete(blog.id)}>Delete</button>
-                    </li>
+                    <div className="card" style={{ width: "18rem" }} key={blog.id}>
+                        <div className="card-body">
+                            <h5 className="card-title">{blog.title}</h5>
+                            <p className="card-text">{blog.body}</p>
+                            {user.title === "Creator" && <button onClick={() => handleDelete(blog.id)}>Delete</button>}
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
+
         </>
     )
 }
